4195d92a7bff2e13b6d00566a60c9287697b05e1
Alexander Bogovich <bogovich.alexander@gmail.com>
2018-05-23 00:23:37 +0700

    Fix log dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     compile(kotlin("stdlib"))
     compile("org.glassfish.jaxb:txw2:2.2.11")
     compile("io.github.microutils:kotlin-logging:1.4.9")
+    compile("org.slf4j:slf4j-simple:1.6.1")
 }
 
 repositories {
